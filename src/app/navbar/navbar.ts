530d21f44666b0a1f8b6706deada5e1909ab6810
Jannis Sabinski <jp.sabinski@gmail.com>
2026-04-10 21:01:20 +0200

    lucide-icons+simple navbar

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { LucideBookImage, LucideCircleUserRound, LucideCompass } from '@lucide/angular';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterModule, LucideCompass, LucideBookImage, LucideCircleUserRound],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
@@ -15,15 +16,12 @@ export class Navbar {
   ) {}
 
   get isLoggedIn() {
-    return this.authService.isLoggedIn();
+    return true;
+    //return this.authService.isLoggedIn();
   }
 
   logout() {
     this.authService.logout();
     this.router.navigate(['/']);
-  }
-
-  goToLogin() {
-    this.router.navigate(['/login']);
   }
 }
