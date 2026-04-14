ca4461521fe7b3dff21edb384000370dc9d059a8
Jannis Sabinski <jp.sabinski@gmail.com>
2026-04-14 12:10:25 +0200

    environment-Anpassung mit public railway backend Url

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,12 @@
 export const environment = {
+  apiURL: 'coding-challenge-engram-backend-production.up.railway.app',
+  bucketUrl: 'https://fytbrrnothhgjtnqqdiq.supabase.co/storage/v1/object/public/pictures/',
+};
+
+/*
+Lokal environment
+export const environment = {
   apiURL: 'http://localhost:3000',
   bucketUrl: 'https://fytbrrnothhgjtnqqdiq.supabase.co/storage/v1/object/public/pictures/',
 };
+*/
