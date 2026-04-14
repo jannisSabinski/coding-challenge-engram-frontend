56db85d35187ffef2768a4666586550a1dadd085
Jannis Sabinski <jp.sabinski@gmail.com>
2026-04-14 12:15:53 +0200

    environment  fix interceptor import

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -1,12 +1,12 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { apiURL } from '../../environment';
+import { environment } from '../../environment';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const authHeader = authService.getAuthHeader();
-  const isApiRequest = req.url.startsWith(apiURL)
+  const isApiRequest = req.url.startsWith(environment.apiURL)
 
   if (authHeader && isApiRequest) {
     req = req.clone({
